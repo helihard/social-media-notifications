57285b48afa9b92ae954c333e20bd7f881b394dd
Heli Hård Källoff <vierge.hard@gmail.com>
2024-09-24 20:36:55 +0200

    render notifications

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,32 +1,111 @@
+import avatarAngelaGray from "../assets/images/avatar-angela-gray.webp";
+import avatarAnnaKim from "../assets/images/avatar-anna-kim.webp";
+import avatarJacobThompson from "../assets/images/avatar-jacob-thompson.webp";
+import avatarKimberlySmith from "../assets/images/avatar-kimberly-smith.webp";
+import avatarMarkWebber from "../assets/images/avatar-mark-webber.webp";
+import avatarNathanPeterson from "../assets/images/avatar-nathan-peterson.webp";
+import avatarRizkyHasanuddin from "../assets/images/avatar-rizky-hasanuddin.webp";
+import chess from "../assets/images/image-chess.webp";
+
+const notifications = [{
+    avatar: avatarMarkWebber,
+    name: "Mark Webber",
+    action: "reacted to your recent post",
+    linkedContent: { type: "post", title: "My first tournament today!" },
+    message: null,
+    isUnread: true,
+    timeSinceAction: "1m ago"
+  },
+  {
+    avatar: avatarAngelaGray,
+    name: "Angela Gray",
+    action: "followed you",
+    linkedContent: null,
+    message: null,
+    isUnread: true,
+    timeSinceAction: "5m ago"
+  },
+  {
+    avatar: avatarJacobThompson,
+    name: "Jacob Thompson",
+    action: "has joined your group",
+    linkedContent: { type: "group", title: "Chess Club" },
+    message: null,
+    isUnread: true,
+    timeSinceAction: "1 day ago"
+  },
+  {
+    avatar: avatarRizkyHasanuddin,
+    name: "Rizky Hasanuddin",
+    action: "sent you a private message",
+    linkedContent: null,
+    message: "Hello, thanks for setting up the Chess Club. I&apos;ve been a member for a few weeks now and I&apos;m already having lots of fun and improving my game.",
+    isUnread: false,
+    timeSinceAction: "5 days ago"
+  },
+  {
+    avatar: avatarKimberlySmith,
+    name: "Kimberly Smith",
+    action: "commented on your picture",
+    linkedContent: { type: "image", src: chess, alt: "A game of chess" },
+    message: null,
+    isUnread: false,
+    timeSinceAction: "1 week ago"
+  },
+  {
+    avatar: avatarNathanPeterson,
+    name: "Nathan Peterson",
+    action: "reacted to your recent post",
+    linkedContent: { type: "post", title: "5 end-game strategies to increase your win rate" },
+    message: null,
+    isUnread: false,
+    timeSinceAction: "2 weeks ago"
+  },
+  {
+    avatar: avatarAnnaKim,
+    name: "Anna Kim",
+    action: "left the group",
+    linkedContent: { type: "group", title: "Chess Club" },
+    message: null,
+    isUnread: false,
+    timeSinceAction: "2 weeks ago"
+  }
+  ]
+
 function MainContainer() {
+
+  function renderLinkedContent(linkedContent) {
+    if (!linkedContent) {
+      return null;
+    } else if (linkedContent.type === "post" || linkedContent.type === "group") {
+      return <a href="#">{linkedContent.title}</a>;
+    } else if (linkedContent.type === "image") {
+      return <img src={linkedContent.src} alt={linkedContent.alt} />;
+    }
+    return null;
+  }
+
   return (
     <>
-    Notifications 3
-
-    Mark all as read
-
-    Mark Webber reacted to your recent post My first tournament today!
-    1m ago
-
-    Angela Gray followed you
-    5m ago
-
-    Jacob Thompson has joined your group Chess Club
-    1 day ago
-
-    Rizky Hasanuddin sent you a private message
-    5 days ago
-    Hello, thanks for setting up the Chess Club. I&apos;ve been a member for a few weeks now and 
-    I&apos;m already having lots of fun and improving my game.
-
-    Kimberly Smith commented on your picture
-    1 week ago
-
-    Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
-    2 weeks ago
-
-    Anna Kim left the group Chess Club
-    2 weeks ago
+      <div id="container">
+        <header>
+          Notifications 3
+          <button>Mark all as read</button>
+        </header>
+        <div>
+          {notifications.map((notification) => (
+            <div key={notification.index}>
+              <img src={notification.avatar} alt={notification.name}/>
+              <div>
+                <div>
+                <div>{notification.name}{notification.action} {renderLinkedContent(notification.linkedContent)}</div>
+                <div>{notification.timeSinceAction}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
