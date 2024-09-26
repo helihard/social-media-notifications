e9057fe2ff16f3755c816094dfb82617669e57a2
Heli Hård Källoff <vierge.hard@gmail.com>
2024-09-26 14:41:08 +0200

    prepare deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/social-media-notifications',
   plugins: [react()],
+  base: '/social-media-notifications',
+  build: {
+    outDir: 'build'
+  }
 })
