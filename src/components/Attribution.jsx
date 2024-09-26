394a8ad06d5ec86c68a84fdc249dd5ba61668676
Heli Hård Källoff <vierge.hard@gmail.com>
2024-09-26 13:26:40 +0200

    finishing css touches, desktop view, attribution

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -3,7 +3,7 @@ function Attribution() {
   return (
     <div className="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
+    Coded by <a href="https://github.com/helihard" target="_blank">Heli Hård</a>.
   </div>
   )
 }
